150979d9f2bcbcaa25bcc44c7052933237fbc2fc
THCSC <tobias@skancode.dk>
2024-07-27 14:38:26 +0200

    add gh service, custom validator and post issue route

--- internal/server.go
+++ internal/server.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/go-playground/validator/v10"
 	_ "github.com/joho/godotenv/autoload"
 
 	"github.com/labstack/echo/v4"
@@ -15,12 +16,22 @@ import (
 
 type Server struct {
 	port int
+
+	ghService GithubService
 }
 
-func NewServer() *http.Server {
-	port, _ := strconv.Atoi(os.Getenv("PORT"))
+func NewServer() (*http.Server, error) {
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		return nil, err
+	}
+	s, err := NewGithubService()
+	if err != nil {
+		return nil, err
+	}
 	NewServer := &Server{
-		port: port,
+		port:      port,
+		ghService: s,
 	}
 
 	// Declare Server config
@@ -32,13 +43,52 @@ func NewServer() *http.Server {
 		WriteTimeout: 30 * time.Second,
 	}
 
-	return server
+	return server, nil
+}
+
+type CustomValidator struct {
+	v *validator.Validate
+}
+
+func (v *CustomValidator) Validate(i interface{}) error {
+	return v.v.Struct(i)
 }
 
 func (s *Server) RegisterRoutes() http.Handler {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Validator = &CustomValidator{v: validator.New()}
+
+	e.POST("/api/:repo/issues", s.PostIssueHandler)
 
 	return e
+}
+
+type postIssueData struct {
+	Repo    string `param:"repo" validate:"required"`
+	Title   string `json:"title" validate:"required"`
+	Options struct {
+		Body      string   `json:"body"`
+		Assignees []string `json:"assignees" validate:"unique"`
+		Milestone string   `json:"milestone"`
+		Labels    []string `json:"labels" validate:"unique"`
+	} `json:"options"`
+}
+
+func (s *Server) PostIssueHandler(c echo.Context) error {
+	var data postIssueData
+	if err := c.Bind(&data); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err)
+	}
+	if err := c.Validate(data); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err)
+	}
+
+	issue, err := s.ghService.CreateIssue("TobiasTheDanish", data.Repo, data.Title, CreateIssueOptions(data.Options))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	return c.JSON(201, issue)
 }
